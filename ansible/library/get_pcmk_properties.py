24ba9a73d78db456c7ce975938c50f018e12e4bd
devanshjain <devanshjain@microsoft.com>
2024-09-23 20:39:55 +0000

    Refactor get_pcmk_properties.py to optimize stonith command retrieval and update for REDHAT

--- ansible/library/get_pcmk_properties.py
+++ ansible/library/get_pcmk_properties.py
@@ -233,17 +233,9 @@ def validate_fence_azure_arm(ansible_os_family: str, virtual_machine_name: str):
             text=True,
             check=True,
         )
-        stonith_config = json.loads(result.stdout)
-        stonith_config_parsed = {}
-        if isinstance(stonith_config, list):
-            for item in stonith_config:
-                if isinstance(item, dict):
-                    stonith_config_parsed = item
-                    break
-            else:
-                raise ValueError("No dictionary found in stonith_config list")
+        stonith_config = json.load(result.stdout)
         nvpairs = (
-            stonith_config_parsed.get("primitives", {})
+            stonith_config.get("primitives", {})
             .get("instance_attributes", {})
             .get("nvpairs", [])
         )
