efb38c086bc41ce3f90727efb1c4c51eae77017d
devanshjain <devanshjain@microsoft.com>
2025-01-15 19:30:10 +0000

    Add constants for cluster resources and parameters in new constants.py file

---
+++ src/library/constants.py
@@ -0,0 +1,211 @@
+SUCCESS_STATUS = "PASSED"
+ERROR_STATUS = "FAILED"
+WARNING_STATUS = "WARNING"
+
+CLUSTER_RESOURCES = {
+    "SUSE": {
+        "ocf:heartbeat:SAPHanaTopology": {
+            "clone-node-max": "1",
+            "target-role": "Started",
+            "interleave": "true",
+            "monitor-interval": "10",
+            "monitor-timeout": "600",
+            "start-interval": "0",
+            "start-timeout": "600",
+            "stop-interval": "0",
+            "stop-timeout": "300",
+        },
+        "ocf:heartbeat:SAPHana": {
+            "notify": "true",
+            "clone-max": "2",
+            "clone-node-max": "1",
+            "target-role": "Started",
+            "interleave": "true",
+            "PREFER_SITE_TAKEOVER": "true",
+            "DUPLICATE_PRIMARY_TIMEOUT": "7200",
+            "AUTOMATED_REGISTER": "true",
+            "start-interval": "0",
+            "start-timeout": "3600",
+            "stop-interval": "0",
+            "stop-timeout": "3600",
+            "promote-interval": "0",
+            "promote-timeout": "3600",
+            "monitor-Master-interval": "60",
+            "monitor-Master-timeout": "700",
+            "monitor-Slave-interval": "61",
+            "monitor-Slave-timeout": "700",
+            "demote-interval": "0s",
+            "demote-timeout": "320",
+        },
+        "stonith:fence_azure_arm": {
+            "pcmk_monitor_retries": "4",
+            "pcmk_action_limit": "3",
+            "power_timeout": "240",
+            "pcmk_reboot_timeout": "900",
+            "pcmk_delay_max": "15",
+            "monitor-interval": "3600",
+            "monitor-timeout": "120",
+        },
+        "ocf:heartbeat:azure-events-az": {
+            "interleave": "true",
+            "allow-unhealthy-nodes": "true",
+            "failure-timeout": "120s",
+            "start-start-delay": "60s",
+            "monitor-interval": "10s",
+        },
+        "ocf:heartbeat:azure-lb": {
+            "monitor-interval": "10s",
+            "monitor-timeout": "20s",
+            "resource-stickiness": "0",
+        },
+        "ocf:heartbeat:IPaddr2": {
+            "monitor-interval": "10s",
+            "monitor-timeout": "20s",
+        },
+    },
+    "REDHAT": {
+        "ocf:heartbeat:SAPHanaTopology": {
+            "clone-node-max": "1",
+            "target-role": "Started",
+            "interleave": "true",
+            "monitor-interval": "10",
+            "monitor-timeout": "600",
+            "start-interval": "0s",
+            "start-timeout": "600",
+            "stop-interval": "0s",
+            "stop-timeout": "300",
+        },
+        "ocf:heartbeat:SAPHana": {
+            "notify": "true",
+            "clone-max": "2",
+            "clone-node-max": "1",
+            "target-role": "Started",
+            "interleave": "true",
+            "PREFER_SITE_TAKEOVER": "true",
+            "DUPLICATE_PRIMARY_TIMEOUT": "7200",
+            "AUTOMATED_REGISTER": "true",
+            "start-interval": "0s",
+            "start-timeout": "3600",
+            "stop-interval": "0s",
+            "stop-timeout": "3600",
+            "promote-interval": "0s",
+            "promote-timeout": "3600",
+            "monitor-Master-interval": "59",
+            "monitor-Master-timeout": "700",
+            "monitor-Slave-interval": "61",
+            "monitor-Slave-timeout": "700",
+            "demote-interval": "0s",
+            "demote-timeout": "3600",
+        },
+        "stonith:fence_azure_arm": {
+            "pcmk_monitor_retries": "4",
+            "pcmk_action_limit": "3",
+            "power_timeout": "240",
+            "pcmk_reboot_timeout": "900",
+            "pcmk_delay_max": "15s",
+            "monitor-interval": "3600",
+            "pcmk_monitor_timeout": "120",
+        },
+        "ocf:heartbeat:azure-events-az": {
+            "interleave": "true",
+            "allow-unhealthy-nodes": "true",
+            "failure-timeout": "120s",
+            "start-start-delay": "60s",
+            "monitor-interval": "10s",
+        },
+        "ocf:heartbeat:azure-lb": {
+            "monitor-interval": "10s",
+            "monitor-timeout": "20s",
+        },
+        "ocf:heartbeat:IPaddr2": {
+            "monitor-interval": "10s",
+            "monitor-timeout": "20s",
+        },
+    },
+}
+CLUSTER_PROPERTIES = {
+    "crm_config": {
+        "cib-bootstrap-options": {
+            "have-watchdog": "false",
+            "cluster-infrastructure": "corosync",
+            "stonith-enabled": "true",
+            "concurrent-fencing": "true",
+            "stonith-timeout": "900s",
+            "maintenance-mode": "false",
+            "azure-events_globalPullState": "IDLE",
+            "priority-fencing-delay": "30",
+        }
+    },
+    "rsc_defaults": {
+        "build-resource-defaults": {
+            "resource-stickiness": "1000",
+            "migration-threshold": "5000",
+            "priority": "1",
+        }
+    },
+    "op_defaults": {
+        "op-options": {
+            "timeout": "600",
+            "record-pending": "true",
+        }
+    },
+}
+
+OS_PARAMETERS = {
+    "REDHAT": {
+        "sysctl": {
+            "net.ipv4.tcp_timestamps": {"expected_value": "1"},
+            "vm.swappiness": {"expected_value": "10"},
+        },
+        "corosync-cmapctl": {
+            "runtime.config.totem.token": {"expected_value": "30000"},
+            "runtime.config.totem.consensus": {"expected_value": "36000"},
+        },
+    },
+    "SUSE": {
+        "sysctl": {
+            "net.ipv4.tcp_timestamps": {"expected_value": "1"},
+            "vm.swappiness": {"expected_value": "10"},
+        },
+        "corosync-cmapctl": {
+            "runtime.config.totem.token": {"expected_value": "30000"},
+            "runtime.config.totem.consensus": {"expected_value": "36000"},
+            "quorum.expected_votes": {"expected_value": "2"},
+        },
+    },
+}
+
+CUSTOM_OS_PARAMETERS = {
+    "REDHAT": {
+        "quorum.expected_votes": {
+            "expected_value": "2",
+            "parameter_name": "Expected votes",
+            "command": ["pcs", "quorum", "status"],
+        },
+    },
+    "SUSE": {},
+}
+
+REQUIRED_PARAMETERS = {
+    "priority-fencing-delay",
+}
+
+CONSTRAINTS = {
+    "rsc_colocation": {
+        "score": "4000",
+        "rsc-role": "Started",
+        "with-rsc-role": "Promoted",
+    },
+    "rsc_order": {
+        "first-action": "start",
+        "then-action": "start",
+        "symmetrical": "false",
+    },
+    "rsc_location": {
+        "score-attribute": "#health-azure",
+        "operation": "defined",
+        "attribute": "#uname",
+    },
+}
+
+PARAMETER_VALUE_FORMAT = "Name: %s, Value: %s, Expected Value: %s"
