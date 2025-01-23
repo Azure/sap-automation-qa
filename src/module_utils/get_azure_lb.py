9eb842cb3d22e624e7a98259e81317bfef03b1a3
devanshjain <devanshjain@microsoft.com>
2025-01-23 06:58:53 +0000

    Remove unused import of cluster constants in get_azure_lb module

--- src/module_utils/get_azure_lb.py
+++ src/module_utils/get_azure_lb.py
@@ -8,7 +8,6 @@ Custom ansible module for getting Azure Load Balancer details
 from ansible.module_utils.basic import AnsibleModule
 from azure.identity import ManagedIdentityCredential
 from azure.mgmt.network import NetworkManagementClient
-from ansible.module_utils.cluster_constants import PROBES, RULES
 
 PROBES = {
     "probe_threshold": 2,
